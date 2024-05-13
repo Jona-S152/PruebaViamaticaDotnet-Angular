278aed32e476494a6412ea0174951359c84eebb8
Jona-S152 <solisjonathan152@gmail.com>
2024-05-13 15:46:14 -0500

    Prueba .NET y Angular - Jonathan Solis

---
+++ Backend/MarketPlaceAPI/MarketPlace.BLL/Services/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using MarketPlace.Models;
+using MarketPlace.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlace.BLL.Services
+{
+    public interface IAuthService
+    {
+        public Usuario Authenticate(UsuarioLoginDTO usuario);
+        public string GetRole(Usuario usuario);
+        public string GenerateToken(Usuario usuario, string rol);
+    }
+}
